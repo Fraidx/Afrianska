badb14fe2f128b31ffa649a1deb8597930fd6985
fraidx <fraidx@mail.ru>
2020-02-05 05:01:30 +0400

    global fix for repository

--- src/JavaScript/Index.js
+++ src/JavaScript/Index.js
@@ -1,24 +1,18 @@
-function modal(){
+function modal() {
+  let modal = document.querySelector(".modal");
+  let closebtn = document.querySelector(".modal__close");
+  let openbtn = document.querySelector(".menu");
 
-    let modal = document.querySelector('.modal');
-    let closebtn = document.querySelector('.close');
-    let openbtn = document.querySelector('#menu');
+  closebtn.addEventListener("click", () => {
+    modal.classList.toggle("opened");
+    openbtn.style.display = "block";
+    document.body.style.overflowY = "auto";
+  });
 
-
-    
-    closebtn.addEventListener('click', () => {
-        modal.classList.toggle('opened')
-        openbtn.style.display = "block"
-        document.body.style.overflowY = "auto";
-    })
-
-    openbtn.addEventListener('click', () => {
-        modal.classList.toggle('opened')
-        openbtn.style.display = "none"
-        document.body.style.overflowY = "hidden";
-    })
-
-
+  openbtn.addEventListener("click", () => {
+    modal.classList.toggle("opened");
+    openbtn.style.display = "none";
+    document.body.style.overflowY = "hidden";
+  });
 }
-
 modal();
